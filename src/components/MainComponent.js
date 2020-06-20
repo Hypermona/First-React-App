209150b7ce41bbdcb2892688ad338c5c49193eef
Hypermona <mohans8050@gmail.com>
2020-06-20 21:01:45 +0530

    SPA part-1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,7 +3,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./HomeComponent";
 import Menu from "./MenuComponent";
 import { DISHES } from "../shared/dishes";
-import DishDtail from "./DishdetailComponent";
+import { COMMENTS } from "../shared/comments";
+import { LEADERS } from "../shared/leaders";
+import { PROMOTIONS } from "../shared/promotions";
+import Contact from "./ContactComponent";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import { Switch, Route, Redirect } from "react-router-dom";
@@ -13,12 +16,21 @@ class Main extends Component {
     super(props);
     this.state = {
       dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS,
     };
   }
 
   render() {
     const HomePage = () => {
-      return <Home />;
+      return (
+        <Home
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
+      );
     };
     return (
       <div>
@@ -30,6 +42,7 @@ class Main extends Component {
             path="/menu"
             component={() => <Menu dishes={this.state.dishes} />}
           />
+          <Route exact path="/Contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
